fbe31683ffb34bb95943430d519530a0021c04ec
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-08-17 16:07:20 -0500

    74. Stock Quote Generator Service. Closes #2

---
+++ src/main/java/da/springframework/mongostockquoteservice/service/QuoteGeneratorService.java
@@ -0,0 +1,11 @@
+package da.springframework.mongostockquoteservice.service;
+
+import da.springframework.mongostockquoteservice.model.Quote;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public interface QuoteGeneratorService {
+
+    Flux<Quote> fetchQuoteStream(Duration period);
+}
